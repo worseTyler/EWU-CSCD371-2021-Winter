cf98fef39b570951e77e51de5dbab6a625c74be6
Tyler Jones <tylerjones321@gmail.com>
2021-01-12 21:55:49 -0800

    Starting to think about what tests there might be

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -6,6 +6,26 @@ namespace Logger.Tests
     [TestClass]
     public class LogFactoryTests
     {
-        
+        public TestContext TestContext {get; set;}
+        public virtual string TestDir { get; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+        }
+
+
+        public void ConfigureFileLogger_GivenValidFile_ReturnsFileLogger()
+        {
+            //Arrange
+            //Act
+
+            //Assert
+        }
+        [TestMethod]
+        public void CreateLogger_GivenFilePath_ReturnsFileLogger()
+        {
+
+        }
     }
 }
