446ad2517b3eec4c7b249672d4c98df9ac1eb91e
Mark Michaelis <mark@IntelliTect.com>
2021-01-14 13:23:16 -0800

    Code at the end of class

---
+++ Lectures/2020.01.12-ObjectOrientedProgramming/Classroom/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Classroom
+{
+    public static class StringExtension
+    {
+        public static string TitleCase(this string value, int maxLength = 0)
+        {
+            return $"{char.ToUpper(value[0])}{value.Substring(1)}";
+
+
+        }
+    }
+}
