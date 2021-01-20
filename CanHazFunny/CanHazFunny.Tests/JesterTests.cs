2db22cd13576bd1a075cc5bd90fee47bf192ac3f
Tyler Jones <tylerjones321@gmail.com>
2021-01-19 21:31:21 -0800

    First version of Jester class with unit tests

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,10 +1,55 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
 
 namespace CanHazFunny.Tests
 {
     [TestClass]
     public class JesterTests
     {
-        
+        [TestMethod]
+        public void TellJoke_WhenCalled_RunsBothInterfacedMethods()
+        {
+            Mock<IJokeService> iJokeService = new();
+            Mock<IOutputJoke> iOutputJoke = new();
+
+            string returnValue = Guid.NewGuid().ToString();
+            iJokeService.Setup(getJoke => getJoke.GetJoke())
+                .Returns(returnValue);
+
+            string retrievedJoke = iJokeService.Object.GetJoke();
+
+            iOutputJoke.Setup(outputJoke => outputJoke.OutputJoke(retrievedJoke));
+
+            Jester jester = new Jester(iJokeService.Object);
+
+            jester.TellJoke();
+
+            Assert.AreEqual(retrievedJoke, returnValue);
+            //iOutputJoke.Verify(outputJoke => outputJoke.OutputJoke(retrievedJoke), Times.Once());
+        }
+
+        [TestMethod]
+        public void TellJoke_GivenChuckNorrisJoke_CyclesThroughJokes()
+        {
+            Mock<IJokeService> iJokeService = new();
+            Mock<IOutputJoke> iOutputJoke = new();
+
+            iJokeService.SetupSequence(getJoke => getJoke.GetJoke())
+                .Returns("If you spell Chuck Norris in Scrabble, you win. Forever.")
+                .Returns("The chief export of Chuck Norris is pain.")
+                .Returns("An actually good joke");
+
+            //iOutputJoke.Setup(outputJoke => outputJoke.OutputJoke("Who Knows"));
+
+            Jester jester = new Jester(iJokeService.Object);
+
+            jester.TellJoke();
+
+            iJokeService.Verify(getJoke => getJoke.GetJoke(), Times.Exactly(3));
+
+
+        }
+
     }
 }
---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CanHazFunny
+{
+    public class Jester : IJokeService, IOutputJoke
+    {
+        private IJokeService IJokeService { get; }
+        //private IOutputJoke IOutputJoke { get; }
+
+        public Jester(IJokeService jokeService /*IOutputJoke outputJoke*/)
+        {
+            IJokeService = jokeService ?? throw new ArgumentNullException((nameof(IJokeService)));
+            //IOutputJoke = outputJoke ?? throw new ArgumentNullException((nameof(IOutputJoke)));
+        }
+        public string GetJoke()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OutputJoke(string joke)
+        {
+            Console.WriteLine(joke);
+        }
+
+        public void TellJoke()
+        {
+            string joke;
+            do
+            {
+                joke = IJokeService.GetJoke();
+            } while (joke.Contains("Chuck Norris"));
+            
+            OutputJoke(joke);
+        }
+    }
+}
