cd2d166764e0f9a9f68f23bbabe49a7c3b87be0d
Tyler Jones <tylerjones321@gmail.com>
2021-02-11 23:40:10 -0800

    CsvRows test and possible implementation

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class SampleDataTests
+    {
+        [TestMethod]
+        public void CsvRowsProperty_GivenFilePath_LoadsDataProperly()
+        {
+            SampleData sampleData = new();
+            IEnumerable<string> enumerable = sampleData.CsvRows;
+
+            Assert.AreEqual("1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Helena,MT,70577", enumerable.First());
+        }
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual<int>(1, 1);
+        }
+
+    }
+}
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public IEnumerable<string> CsvRows
+        {
+            get
+            {
+                string[] lines = File.ReadAllLines("../../../../Assignment/People.csv");
+                return lines.Skip(1).Distinct();
+            }
+        }
 
         // 2.
         public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
