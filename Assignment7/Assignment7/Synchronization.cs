b447c515c094e08a0848c7e66ce675ba73c2f61c
Tyler Jones <tylerjones321@gmail.com>
2021-03-04 01:16:36 -0800

    Async is weird

---
+++ Assignment7/Assignment7/Synchronization.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Assignment7
+{
+    [TestClass]
+    public class Synchronization
+    {
+        [TestMethod]
+        public void Unsynchronized_GivenLargeEnoughInput_IsNotSynchronized()
+        {
+            string[] input = { "1000000" };
+
+            Assert.AreNotEqual<int>(0, Program.Unsynchronized(input));
+            // This passing shows that it is not synchronized
+        }
+
+        [TestMethod]
+        public void SynchronizedLock_GivenLargeInput_OutputIsZero()
+        {
+            string[] input = { "1000000" };
+            Assert.AreEqual<int>(0, Program.SynchronizedLock(input));
+        }
+
+        [TestMethod]
+        public void SynchronizedInterlocked_GivenLargeInput_OutputIsZero()
+        {
+            string[] input = { "1000000" };
+            Assert.AreEqual<int>(0, Program.SynchronizedInterlocked(input));
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            string[] input = { "1000000" };
+            Assert.AreEqual<int>(1000000, Program.SynchronizedThreadLocal(input));
+            // The threads are entirely separate and cannont affect each other
+            // if this wasn't the case then the actual value you be less than the
+            // expected value
+        }
+    }
+
+
+    public class Program
+    {
+        static int _Total = int.MaxValue;
+        static int _Count = 0;
+
+        public static int Unsynchronized(string[] args)
+        {
+            if (args?.Length > 0) { int.TryParse(args[0], out _Total); }
+
+            Console.WriteLine($"Increment and decrementing {_Total} times...");
+
+            // Use Task.Factory.StartNew for .NET 4.0
+            Task task = Task.Run(() => Decrement());
+
+            // Increment
+            for (int i = 0; (i < _Total); i++)
+            {
+                _Count++;
+            }
+
+            task.Wait();
+            Console.WriteLine($"Count = {_Count}");
+
+            return _Count;
+        }
+
+        static void Decrement()
+        {
+            // Decrement
+            for (int i = 0; i < _Total; i++)
+            {
+                _Count--;
+            }
+        }
+        readonly static object Lock = new object();
+        public static int SynchronizedLock(string[] args)
+        {
+            if (args?.Length > 0) { int.TryParse(args[0], out _Total); }
+
+            Console.WriteLine($"Increment and decrementing {_Total} times...");
+
+            // Use Task.Factory.StartNew for .NET 4.0
+            Task task = Task.Run(() => DecrementLock());
+
+            // Increment
+            for (int i = 0; (i < _Total); i++)
+            {
+                lock (Lock)
+                {
+                    _Count++;
+                }
+            }
+
+            task.Wait();
+            Console.WriteLine($"Count = {_Count}");
+
+            return _Count;
+        }
+
+        static void DecrementLock()
+        {
+            // Decrement
+            for (int i = 0; i < _Total; i++)
+            {
+                lock (Lock)
+                {
+                    _Count--;
+                }
+            }
+        }
+
+        public static int SynchronizedInterlocked(string[] args)
+        {
+            if (args?.Length > 0) { int.TryParse(args[0], out _Total); }
+
+            Console.WriteLine($"Increment and decrementing {_Total} times...");
+
+            // Use Task.Factory.StartNew for .NET 4.0
+            Task task = Task.Run(() => DecrementInterlocked());
+
+            // Increment
+            for (int i = 0; (i < _Total); i++)
+            {
+                Interlocked.Increment(ref _Count);
+            }
+
+            task.Wait();
+            Console.WriteLine($"Count = {_Count}");
+
+            return _Count;
+        }
+
+        static void DecrementInterlocked()
+        {
+            // Decrement
+            for (int i = 0; i < _Total; i++)
+            {
+                Interlocked.Decrement(ref _Count);
+            }
+        }
+
+
+        static ThreadLocal<int> CountLocal = new();
+
+        public static int Count
+        {
+            get { return CountLocal.Value; }
+            set { CountLocal.Value = value; }
+        }
+
+        public static int SynchronizedThreadLocal(string[] args)
+        {
+            if (args?.Length > 0) { int.TryParse(args[0], out _Total); }
+
+            Console.WriteLine($"Increment and decrementing {_Total} times...");
+
+            // Use Task.Factory.StartNew for .NET 4.0
+            Task task = Task.Run(() => DecrementThreadLocal());
+
+            // Increment
+            for (int i = 0; (i < _Total); i++)
+            {
+                Count++;
+            }
+
+            task.Wait();
+            Console.WriteLine($"Count = {Count}");
+
+            return Count;
+        }
+
+        static void DecrementThreadLocal()
+        {
+            // Decrement
+            for (int i = 0; i < _Total; i++)
+            {
+                Count--;
+            }
+        }
+    }
+}
--- Assignment7/Assignment7/TaskBasedProgramming.cs
+++ Assignment7/Assignment7/TaskBasedProgramming.cs
@@ -3,82 +3,182 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
+[assembly: Parallelize(Workers = 4, Scope = ExecutionScope.ClassLevel)] // I found this online to run tests in parallel but I'm not sure where to put it
 namespace Assignment7
 {
     [TestClass]
     public class TaskBasedProgramming
     {
-        [TestMethod]
-        public async Task TestMethod1()
+        private async Task<int> DownloadTextAsync(params string[] urls)
         {
+            if (urls is null)
+                throw new ArgumentNullException($"DownloadTextAsync: {nameof(urls)} was null");
 
-            IEnumerable<string> urls = Enumerable.Repeat<string>("http://intellitect.com", 15);
+            Task<int> task = Task.Run(() =>
+            {
+                int numChars = 0;
+                foreach (string url in urls)
+                {
+                    WebClient webClient = new();
+                    string output = webClient.DownloadString(url);
+                    numChars += output.Length;
+                }
+                return numChars;
+            });
 
-			int thing1 = await DownloadTextAsync(urls.ToArray());
-			Console.WriteLine($"Thing 1: Number of characters: {thing1}");
-			
-			int thing2 = DownloadText(urls.ToArray());
-			Console.WriteLine($"Thing 2: Number of characters: {thing2}");
-
-			int thing3 = await DownloadTextRepeatedlyAsync(5, urls.ToArray());
-			Console.WriteLine($"Thing 3: Number of characters: {thing3}");
-
-		}
-
-		public async Task<int> DownloadTextAsync(params string[] urls)
-        {
-			Task<int> task = Task.Run(() =>
-			{
-				int numChars = 0;
-				foreach (string url in urls)
-				{
-
-					Console.WriteLine($"Calling URL: {url} - {System.DateTime.Now}");
-					WebClient webClient = new();
-					DateTime begin = System.DateTime.Now;
-					string output = webClient.DownloadString(url);
-					DateTime end = System.DateTime.Now;
-					Console.WriteLine($"URL call took: {(end - begin).TotalSeconds} seconds.");
-					numChars += output.Length;
-				}
-				return numChars;
-			});
-
-			return await task;
+            return await task;
         }
 
-		public async Task<int> DownloadTextRepeatedlyAsync(int repetitions, params string[] urls)
-		{
-			return await Task.Run(() =>
-			{
-				int numChars = 0;
+        public async Task<int> DownloadTextRepeatedlyAsync(int repetitions, IProgress<double> progress, ParallelOptions parallelOptions, params string[] urls)
+        {
+            if (urls is null)
+                throw new ArgumentNullException($"DownloadTextAsync: {nameof(urls)} was null");
+            if (repetitions < 0)
+                throw new ArgumentException($"DownloadTextAsync: Doesn't make sense to have less than zero repetitions ({repetitions})");
 
-                ParallelLoopResult x = Parallel.For(0, repetitions, async index =>
-				{
-					numChars += await DownloadTextAsync(urls);
-				});
+            Task<int> task = Task.Run(() =>
+            {
+                int numChars = 0;
 
-				return numChars;
-			});
-		}
+                Parallel.For(0, repetitions, parallelOptions, index =>
+                {
+                    parallelOptions.CancellationToken.ThrowIfCancellationRequested();
+                    // numChars += DownloadTextAsync(urls).Result;  //Thread synchronization problem
+                    //Interlocked.Add(ref numChars, await DownloadTextAsync(urls)); // When called with async action, the Parallel.For does not wait for the function to return, just the invocation of the method
+                    Interlocked.Add(ref numChars, DownloadTextAsync(urls).Result);
 
-		public int DownloadText(params string[] urls)
-		{
-			int numChars = 0;
-			foreach (string url in urls)
-			{
-				Console.WriteLine($"Calling URL: {url} - {System.DateTime.Now}");
-				WebClient webClient = new();
-				DateTime begin = System.DateTime.Now;
-				string output = webClient.DownloadString(url);
-				DateTime end = System.DateTime.Now;
-				Console.WriteLine($"URL call took: {(end - begin).TotalSeconds} seconds.");
-				numChars += output.Length;
-			}
-			return numChars;
-		}
+                    progress.Report(((double)index + 1) / repetitions);
 
-	}
+                });
+
+                return numChars;
+            });
+
+            return await task;
+        }
+
+        [TestMethod]
+        public void DownloadTextAsync_GivenValidParams_ReturnsREASONABLEOutput()
+        {
+
+            IEnumerable<string> urls = Enumerable.Repeat<string>("http://google.com", 15); // I really don't like using this because it keeps changing
+            int lowerBound = 700000;
+            int upperBound = 800000;
+
+            int actual = DownloadTextAsync(urls.ToArray()).Result;
+
+            Console.WriteLine($"Output: Number of characters: {actual}");
+
+            bool result = (actual < upperBound && actual > lowerBound);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void DownloadTextAsync_GivenNullParams_ThrowsException()
+        {
+            bool isThrown = false;
+            try
+            {
+                DownloadTextAsync(null!).Wait();
+            }
+            catch (AggregateException aggregateException)
+            {
+                aggregateException.Handle(exception =>
+                {
+                    if (exception is ArgumentNullException)
+                        isThrown = true;
+                    return true;
+                });
+            }
+            Assert.IsTrue(isThrown);
+        }
+
+        [TestMethod]
+        public void DownloadTextRepeatedlyAsync_GivenRelativelyStaticSite_ReturnsExpectedValue()
+        {
+            IEnumerable<string> urls = Enumerable.Repeat<string>("http://intellitect.com", 15);
+
+            int actual = DownloadTextRepeatedlyAsync(5, new Progress<double>(progress => Console.WriteLine($"Progress: {progress * 100}%")), new ParallelOptions(), urls.ToArray()).Result;
+            int lowerBound = 10000000;
+            int upperBound = 13000000;
+            Console.WriteLine(actual);
+
+            bool result = (actual < upperBound && actual > lowerBound);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void DownloadTextRepeatedlyAsync_GivenNullParams_ThrowsException()
+        {
+            bool isThrown = false;
+            try
+            {
+                DownloadTextRepeatedlyAsync(5, new Progress<double>(progress => Console.WriteLine($"Progress: {progress * 100}%")), new ParallelOptions(), null!).Wait();
+            }
+            catch (AggregateException aggregateException)
+            {
+                aggregateException.Handle(exception =>
+                {
+                    if (exception is ArgumentNullException)
+                        isThrown = true;
+                    return true;
+                });
+            }
+            Assert.IsTrue(isThrown);
+        }
+
+        [TestMethod]
+        public void DownloadTextRepeatedlyAsync_GivenInvalidRepetitions_ThrowsException()
+        {
+            IEnumerable<string> urls = Enumerable.Repeat<string>("http://intellitect.com", 15);
+            bool isThrown = false;
+            try
+            {
+                DownloadTextRepeatedlyAsync(-1, new Progress<double>(progress => Console.WriteLine($"Progress: {progress * 100}%")), new ParallelOptions(), urls.ToArray()).Wait();
+            }
+            catch (AggregateException aggregateException)
+            {
+                aggregateException.Handle(exception =>
+                {
+                    if (exception is ArgumentException)
+                        isThrown = true;
+                    return true;
+                });
+            }
+            Assert.IsTrue(isThrown);
+        }
+
+        [TestMethod]
+        public void DownloadTextRepeatedlyAsycn_LoopCanBeCancelled()
+        {
+            IEnumerable<string> urls = Enumerable.Repeat<string>("http://intellitect.com", 15);
+            CancellationTokenSource cts = new CancellationTokenSource();
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.CancellationToken = cts.Token;
+
+            bool canceled = false;
+
+            Task task = Task.Run(() =>
+            {
+                cts.Cancel();
+            });
+            try
+            {
+                int actual = DownloadTextRepeatedlyAsync(5, new Progress<double>(progress => Console.WriteLine($"Progress: {progress * 100}%")), parallelOptions, urls.ToArray()).Result;
+            }
+            catch(AggregateException aggregateException)
+            {
+                aggregateException.Handle(exception =>
+                {
+                    if (exception is OperationCanceledException)
+                        canceled = true;
+                    return true;
+                });
+            }
+            Assert.IsTrue(canceled);
+        }
+    }
 }
