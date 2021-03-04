e0b9fc8553e2741625a6acf390475d95c0102c9e
Kevin Bost <kitokeboo@gmail.com>
2021-03-04 15:57:22 -0800

    Building login screen

--- Lectures/2021.03.02-WPFEvents/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ Lectures/2021.03.02-WPFEvents/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,123 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.DirectoryServices;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
+﻿using System.Windows;
 
 namespace WpfApp1
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public delegate void MyAction(string s);
-        public event MyAction MyEvent;
-
-        private Timer Timer { get; }
-        private DispatcherTimer DispatcherTimer { get; }
-
         public MainWindow()
         {
+            DataContext = new MainWindowViewModel();
             InitializeComponent();
-            Action action = () => { };
-
-            MyAction myAction = new MyAction(s => { });
-
-            int count = 0;
-            var handler1 = new MyAction(s =>
-            {
-                count++;
-            });
-
-            MyEvent += handler1;
-            MyEvent -= handler1;
-            MyEvent?.Invoke("42");
-
-            MyTextBlock.Text = $"Count is {count}";
-
-            //MyButton.Click += MyButton_Click;
-
-            //Timer = new Timer(OnTimerTick, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
-
-            DispatcherTimer = new();
-            DispatcherTimer.Interval = TimeSpan.FromSeconds(1);
-            DispatcherTimer.Tick += DispatcherTimer_Tick;
-            //DispatcherTimer.Start();
-
-        }
-
-        private void DispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            MyTextBlock.Text = $"{DateTime.Now}";
-        }
-
-        private void OnTimerTick(object state)
-        {
-            Dispatcher.Invoke(() => MyTextBlock.Text = $"{DateTime.Now}");
-        }
-
-        private int Counter { get; set; }
-        private void MyButton_Click(object sender, RoutedEventArgs e)
-        {
-            //await Task.Delay(50);
-            var counter = Counter++;
-            MyTextBlock.Text = $"{counter}";
-            MyListBox.Items.Add($"{counter}");
-            //try
-            //{
-            //await HandleButtonClick();
-            //}
-            //catch(Exception)
-            //{
-            //    //TODO Logging etc
-            //}
-        }
-
-        private async Task HandleButtonClick()
-        {
-            MyTextBlock.Text = $"Waiting...";
-
-            //await Task.Delay(TimeSpan.FromSeconds(1));
-            await Task.Run(() => 
-            {
-                //Cannot do this on a background thread!
-                //MyTextBlock.Text = $"Doing work...";
-
-                Dispatcher.Invoke(() => MyTextBlock.Text = $"Doing work...");
-            
-                //Contemplate the meaning of life
-                Thread.Sleep(1000);
-            });
-
-            MyTextBlock.Text = $"Done";
-        }
-
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void MyListBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            e.Handled = true;
-            if (e.Key == Key.Delete)
-            {
-                MyListBox.Items.Remove(MyListBox.Items.OfType<object>().Last());
-            }
         }
     }
 }
---
+++ Lectures/2021.03.02-WPFEvents/WpfApp1/WpfApp1/MainWindowViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RelayCommand LoginCommand { get; }
+        public ICommand LogoutCommand { get; }
+
+        private string _UserName;
+        public string UserName
+        {
+            get => _UserName;
+            set
+            {
+                if (SetProperty(ref _UserName, value))
+                {
+                    LoginCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        private string _Password = "S3cr3t";
+        public string Password
+        {
+            get => _Password;
+            set
+            {
+                if (SetProperty(ref _Password, value))
+                {
+                    LoginCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        private bool _IsLoggedIn;
+        public bool IsLoggedIn
+        {
+            get => _IsLoggedIn;
+            set => SetProperty(ref _IsLoggedIn, value);
+        }
+
+        private bool SetProperty<T>(ref T field, T newValue, [CallerMemberName]string propertyName = "")
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+
+        public MainWindowViewModel()
+        {
+            LoginCommand = new RelayCommand(DoLogin, CanLogin);
+        }
+        //We do login here... trust me
+        public void DoLogin()
+        {
+            if (string.Equals(UserName, "Kevin", StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(Password, "S3cr3t", StringComparison.Ordinal))
+            {
+                IsLoggedIn = true;
+            }
+            IsLoggedIn = false;
+        }
+
+        public bool CanLogin()
+        {
+            return !string.IsNullOrWhiteSpace(UserName) &&
+                   !string.IsNullOrWhiteSpace(Password);
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action ExecuteDelegate { get; }
+        private Func<bool> CanExecuteDelegate { get; }
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => CanExecuteDelegate.Invoke();
+        public void Execute(object parameter) => ExecuteDelegate.Invoke();
+        public RelayCommand(Action executeDelegate, Func<bool> canExecuteDelegate)
+        {
+            ExecuteDelegate = executeDelegate ?? throw new ArgumentNullException(nameof(executeDelegate));
+            CanExecuteDelegate = canExecuteDelegate ?? throw new ArgumentNullException(nameof(canExecuteDelegate));
+        }
+    }
+}
